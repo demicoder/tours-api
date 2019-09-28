3520f3fe269329ce7644864eb40dbd611cf90f51
Maverick <bredren10@gmail.com>
2019-09-28 20:21:40 +0100

    Get All Tours

--- app.js
+++ app.js
@@ -4,4 +4,16 @@ const express = require('express');
 
 const app = express();
 
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours.json`));
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours
+    }
+  });
+});
+
 app.listen(3000);
