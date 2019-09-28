7425e1e0c890aac25fe0a29a806a8f5344a1a723
Maverick <bredren10@gmail.com>
2019-09-28 22:05:35 +0100

    Post/save tour to file

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours.json`));
 
 app.get('/api/v1/tours', (req, res) => {
@@ -14,6 +16,27 @@ app.get('/api/v1/tours', (req, res) => {
       tours
     }
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = +[tours.length] + 1;
+
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  const updatedTours = [...tours, newTour];
+
+  fs.writeFile(
+    `${__dirname}/data/tours.json`,
+    JSON.stringify(updatedTours),
+    err => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour
+        }
+      });
+    }
+  );
 });
 
 app.listen(3000);
