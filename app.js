58a2bfce5e972b0e97d8fab91c462c03d5ed1339
Maverick <bredren10@gmail.com>
2019-09-28 23:38:13 +0100

    Get single tour resource

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.use(express.json());
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours.json`));
 
 app.get('/api/v1/tours', (req, res) => {
+  if (!tours) {
+    return res.status(404).json({
+      status: 'error',
+      messsage: 'tours not found'
+    });
+  }
+
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -37,6 +44,23 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
+});
+
+app.get(`/api/v1/tours/:id`, (req, res) => {
+  const tourId = +req.params.id;
+  const tour = tours.find(t => t.id === tourId);
+
+  if (!tour) {
+    return res.status(404).json({
+      status: 'error',
+      messsage: 'tour not found'
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    tour
+  });
 });
 
 app.listen(3000);
