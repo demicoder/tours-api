b061908a96820736c4234c7f855e744779485706
Maverick <bredren10@gmail.com>
2019-10-01 22:45:44 +0100

    Add more fields to the tour model/schema

--- models/Tour.js
+++ models/Tour.js
@@ -2,20 +2,58 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
-const tourSchema = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Tour must have a name'],
-    unique: true
+const tourSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Tour must have a name'],
+      unique: true
+    },
+    price: {
+      type: Number,
+      required: [true, 'Tour price must be specified']
+    },
+    duration: {
+      type: Number,
+      required: [true, 'Tour must have a duration']
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'Tour must have a group size']
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'Tour must have a difficulty level']
+    },
+    ratingAverage: {
+      type: Number,
+      default: 0
+    },
+    ratingQuantity: {
+      type: Number,
+      default: 0
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'Tour must have a summary']
+    },
+    description: {
+      type: String,
+      trim: true
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'Tour must have a cover image']
+    },
+    images: [String],
+    startDates: [Date]
   },
-  price: {
-    type: Number,
-    required: [true, 'Tour price must be specified']
-  },
-  rating: {
-    type: Number,
-    default: 0
+  {
+    timestamps: true
   }
-});
+);
 
-module.exports = mongoose.model('Tour', tourSchema);
+const Tour = mongoose.model('Tour', tourSchema);
+module.exports = Tour;
