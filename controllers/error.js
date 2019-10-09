daab54f1347c45535c364bda0c390feb6bc83b54
Maverick <bredren10@gmail.com>
2019-10-09 13:21:47 +0100

    Handle database ID cast error

--- controllers/error.js
+++ controllers/error.js
@@ -1,11 +1,28 @@
+const AppError = require('./../utils/appError');
+
+const handleCastErrorDB = err => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 404);
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Send Error Prod
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  console.log(err);
+  let error = { ...err };
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  });
+  if (error.name === 'CastError') error = handleCastErrorDB(error);
+  sendErrorDev(error, res);
 };
