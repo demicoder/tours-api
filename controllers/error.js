8b685b0b7c5016c3487cca4669c06bd3466edd60
Maverick <bredren10@gmail.com>
2019-10-09 13:43:57 +0100

    Handle validation errors

--- controllers/error.js
+++ controllers/error.js
@@ -11,6 +11,12 @@ const handleDuplicateError = err => {
   return new AppError(message, 400);
 };
 
+const handleValidationError = err => {
+  const errors = Object.values(err.errors).map(el => el.message);
+  const message = `Invalid input data. ${errors.join('. ')} `;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -28,8 +34,8 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   let error = { ...err };
-
   if (error.name === 'CastError') error = handleCastErrorDB(error);
   if (error.code === 11000) error = handleDuplicateError(error);
+  if (error.name === 'ValidatorError') error = handleValidationError(error);
   sendErrorDev(error, res);
 };
