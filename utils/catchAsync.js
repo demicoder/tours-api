780980e89a3e3f8bd1821e75268dabb738ffd1e9
Maverick <bredren10@gmail.com>
2019-11-01 00:03:48 +0100

    Resolve Handler utility helper

--- utils/catchAsync.js
+++ utils/catchAsync.js
@@ -1,3 +1,2 @@
-module.exports = fn => {
-  return (req, res, next) => fn(req, res, next).catch(err => next(err));
-};
+module.exports = fn => (req, res, next) =>
+  Promise.resolve(fn(req, res, next)).catch(err => next(err));
